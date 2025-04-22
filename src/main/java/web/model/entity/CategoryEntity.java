66ada0dc5bf2e25c881da94029b1245b79cf9c00
magnoria <sidaumv@gmail.com>
2025-04-22 15:52:11 +0900

    2025-04-22 수업 app Entity JPA table 양방향 연결

---
+++ src/main/java/web/model/entity/CategoryEntity.java
@@ -0,0 +1,26 @@
+package web.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity@Table(name = "category") //테이블 생성과 매핑
+@Builder
+@Getter@Setter@AllArgsConstructor@NoArgsConstructor // 롬복
+public class CategoryEntity {
+
+    @Id //preimary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+    private long cno; //카테고리 식별번호
+
+    @Column(nullable = false , length = 100) // not null, 길이 100
+    private String cname; //카테고리 이름
+
+    // * 양방향
+    @OneToMany(mappedBy = "categoryEntity" , cascade = CascadeType.ALL , fetch = FetchType.LAZY)
+    @ToString.Exclude@Builder.Default
+    private List<ProductEntity> productEntityList = new ArrayList<>();
+
+}
--- src/main/java/web/model/entity/MemberEntity.java
+++ src/main/java/web/model/entity/MemberEntity.java
@@ -1,11 +1,11 @@
 package web.model.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import web.model.dto.MemberDto;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "member")
@@ -19,6 +19,19 @@ public class MemberEntity {
     private String mpwd;
     private String mname;
     //entity --> dto
+
+    // * 양방향 : FK 엔티티를 여러개 가지므로 List 타입으로 만든다.
+    @OneToMany(mappedBy = "memberEntity" , cascade = CascadeType.ALL , fetch = FetchType.LAZY) // 필드명을 넣어줘야한다.
+    @ToString.Exclude //양방향 설계시 toString 롬복의 순환참조 방지
+    @Builder.Default // 엔티티 객체 생성시 빌드 메소드 사용하면 기본값 을 넣어줌
+    private List<ProductEntity> prodeuctEntityList = new ArrayList<>();
+
+    //@OneToMany(mappedBy = "FK엔티티자바필드명")
+    @OneToMany(mappedBy = "memberEntity" , cascade = CascadeType.ALL , fetch = FetchType.LAZY) // 필드명을 넣어줘야한다.
+    @ToString.Exclude@Builder.Default
+    private List<ReplyEntity> replyEntityList = new ArrayList<>();
+
+
 
     public MemberDto toDto(){
         return  MemberDto.builder() // 여기가 틀렸어서 안됬었음
